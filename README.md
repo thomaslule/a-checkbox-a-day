b69687b498a0deb43d974e87c3b20f77136874e5
Thomas Lulé <thomas@lule.fr>
2015-02-26 19:27:42 +0100

    get rid of generated jade files

--- README.md
+++ README.md
@@ -6,6 +6,5 @@ Learning javascript and node.js by implementing a todolist vaguely inspired by t
     cp local.json-dist local.json # db credentials go here
     npm install
     node bin/init_storage.js
-    node bin/init_jade.js
     npm -g install nodemon
     npm start
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var nconf = require('nconf');
 var bodyParser = require('body-parser');
+var fs = require('fs-extra');
+var jade = require('jade');
 
 nconf.argv().env().file('local.json');
 
@@ -17,6 +19,7 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 // output indented html
 app.locals.pretty = true;
+var jadeTemplates = fs.readFileSync('node_modules/jade/runtime.js').toString() + jade.compileFileClient('views/task.jade', {name: 'jadeTaskTemplate'});
 
 var monthController = require('./controllers/monthController.js');
 
@@ -24,7 +27,11 @@ app.get('/', monthController.get)
 .get('/all', monthController.getAll)
 .post('/new', monthController.postNew)
 .post('/edit', monthController.postEdit)
-.post('/delete', monthController.postDelete);
+.post('/delete', monthController.postDelete)
+.get('/jade_templates.js', function(req, res) {
+    res.type('text/javascript');
+    res.send(jadeTemplates);
+});
 
 var applicationHealth = require('./controllers/healthController.js');
 
