b5aa7b59b0668ce8639a36fc7f823d9334ca08b0
Thomas Lulé <thomas@lule.fr>
2015-03-01 22:28:23 +0100

    show/hide using class

--- public/js/task.js
+++ public/js/task.js
@@ -12,20 +12,16 @@ $.fn.task = function(method, arg) {
     };
 
     methods.edit = function() {
-        $('.task').each(function() {
+        $('.task.editing').removeClass(function() {
             $(this).task('cancelEdit');
         });
-        taskElt.find('.checkbox').hide();
-        taskElt.find('.edit-item-input').show();
+        taskElt.addClass('editing');
         return taskElt;
     };
 
     methods.cancelEdit = function() {
-        if (taskElt.find('.edit-item-input').is(':visible')) {
-            taskElt.find('input[name="name"]').val(taskElt.find('.task-name').text());
-            taskElt.find('.edit-item-input').hide();
-            taskElt.find('.checkbox').show();
-        }
+        taskElt.find('input[name="name"]').val(taskElt.find('.task-name').text());
+        taskElt.removeClass('editing');
     };
 
     if (method == undefined) {
@@ -74,8 +70,7 @@ $(document).on('click', '.task .delete-item-button', function() {
 $(document).on('submit', '.task form', function() {
     var taskElt = $(this).closest('.task');
     taskElt.find('.task-name').text(taskElt.find('input[name="name"]').val());
-    taskElt.find('.edit-item-input').hide();
-    taskElt.find('.checkbox').show();
+    taskElt.removeClass('editing');
     taskElt.trigger('applyEdit');
     return false;
 });
