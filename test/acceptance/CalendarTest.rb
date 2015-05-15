6fc6daa1b600f7477ecd7bc0936796550b7a607d
Thomas Lulé <thomas@lule.fr>
2015-05-15 14:53:18 +0200

    fix edit calendar text

--- test/acceptance/CalendarTest.rb
+++ test/acceptance/CalendarTest.rb
@@ -7,9 +7,15 @@ class MonthTest < AcadTest
     end
 
     def test_edit_day
+        edit_day('modified')
+        edit_day('remodified')
+    end
+
+    def edit_day(text)
         day = $driver.find_element(:css, '.day[data-id="2050-01-05"] .day-text')
+        day.clear;
         day.click
-        $driver.action.send_keys(day, 'modified').send_keys(day, :return).perform
-        check_persisted { assert_equal('modified', $driver.find_element(:css, '.day[data-id="2050-01-05"] .day-text').text, 'calendar not saved') }
+        $driver.action.send_keys(day, text).send_keys(day, :return).perform
+        check_persisted { assert_equal(text, $driver.find_element(:css, '.day[data-id="2050-01-05"] .day-text').text, 'calendar not saved') }
     end
 end
