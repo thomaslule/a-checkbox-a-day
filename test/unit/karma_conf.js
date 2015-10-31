4aece734a1fcffddc7c964b97a26ad99c92b3639
unknown <thomas@lule.fr>
2015-10-31 12:07:21 +0100

    add ngResource to devDependencies

--- test/unit/karma_conf.js
+++ test/unit/karma_conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/angular/angular.js',
+      'node_modules/angular-resource/angular-resource.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'public/**/*.js',
       'test/unit/**/*Test.js'
