4c8db6e0b87e33b21ce26040c15a1326f9970f1b
unknown <thomas@lule.fr>
2017-05-24 00:17:03 +0200

    plug to the webapi

--- api/src/main/java/fr/lule/acad/web/AcadServer.java
+++ api/src/main/java/fr/lule/acad/web/AcadServer.java
@@ -1,13 +1,23 @@
 package fr.lule.acad.web;
 
+import fr.lule.acad.projection.TaskList;
+import fr.lule.acad.stream.EventsBus;
+import fr.lule.acad.stream.MemoryEventStream;
 import net.codestory.http.WebServer;
 
 public class AcadServer {
 
 	public static void main(String[] args) {
+		
+		MemoryEventStream stream = new MemoryEventStream();
+		TaskList list = new TaskList(stream.getHistory());
+		EventsBus bus = new EventsBus(stream);
+		bus.subscribe(list);
+		
 		new WebServer()
-		.configure(routes ->
-		routes.add(new GetTasksForMonth()))
+		.configure(routes -> {
+			routes.add(new TasksController(list, bus));
+		})
 		.start();
 	}
 	
---
+++ api/src/main/java/fr/lule/acad/web/TasksController.java
@@ -0,0 +1,41 @@
+package fr.lule.acad.web;
+
+import java.util.List;
+
+import fr.lule.acad.aggregate.Task;
+import fr.lule.acad.projection.TaskDisplayed;
+import fr.lule.acad.projection.TaskList;
+import fr.lule.acad.stream.IEventPublisher;
+import net.codestory.http.annotations.Get;
+import net.codestory.http.annotations.Post;
+import net.codestory.http.annotations.Prefix;
+import net.codestory.http.payload.Payload;
+
+@Prefix("/api")
+public class TasksController {
+	
+	private TaskList list;
+	private IEventPublisher publisher;
+
+	public TasksController(TaskList list, IEventPublisher publisher) {
+		this.list = list;
+		this.publisher = publisher;
+	}
+
+	@Get("/Tasks")
+	public List<TaskDisplayed> getTasks() {
+		return list.getList();
+	}
+	
+	@Post("/AddTask")
+	public Payload addTask(AddTaskCommand command) {
+		Task.add(publisher, command.todo);
+		return Payload.created();
+	}
+	
+	private static class AddTaskCommand {
+		public String todo;
+	}
+	
+
+}
