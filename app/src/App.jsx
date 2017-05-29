031f37070419fac79dbf60da559eef961d6f56d3
unknown <thomas@lule.fr>
2017-05-29 15:21:47 +0200

    checkboxes!

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -12,10 +12,17 @@ class App extends Component {
     }
 
     render() {
-        console.log( this.state.tasks );
-        var taskList = this.state.tasks.map( function( task ) {
+        var taskList = this.state.tasks.map( ( task ) => {
             return (
-                <li>{task.todo}</li>
+                <li key={task.id}>
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        disabled={task.completed}
+                        onChange={() => this.handleTaskComplete( task )}
+                    />
+                    {task.todo}
+                </li>
             );
         } );
         return (
@@ -49,13 +56,21 @@ class App extends Component {
             .then( tasks => {
                 this.setState( { tasks } );
             } )
-            .catch( error => {
-                console.warn( error );
-            } );
+            .catch( error => console.warn( error ) );
     }
 
     handleChange( event ) {
         this.setState( { newTaskTodo: event.target.value } );
+    }
+
+    handleTaskComplete( task ) {
+        fetch( "api/CompleteTask",
+            {
+                method: "POST",
+                body: JSON.stringify( { id: task.id } )
+            } )
+            .then(() => this.updateTaskList() )
+            .catch( error => console.warn( error ) );
     }
 
     handleClick() {
@@ -68,9 +83,7 @@ class App extends Component {
                 this.setState( { newTaskTodo: "" } );
                 this.updateTaskList();
             } )
-            .catch( error => {
-                console.warn( error );
-            } );
+            .catch( error => console.warn( error ) );
     }
 }
 
